4ed4155207500bb4ea6010fee8cd47e63ee4b75b
DecoyTNT <42523466+DecoyTNT@users.noreply.github.com>
2020-03-31 17:10:18 -0600

    Primeros pasos con el state de React

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
 function App() {
+    // Crear listado de productos
+    const [productos, guardarProductos] = useState([
+        { id: 1, nombre: 'Camisa RectJS', precio: 50 },
+        { id: 2, nombre: 'Camisa AngularJS', precio: 40 },
+        { id: 3, nombre: 'Camisa VueJs', precio: 30 },
+        { id: 4, nombre: 'Camisa Node', precio: 20 }
+    ]);
+
     // Obtener el año
     const fecha = new Date().getFullYear();
 
