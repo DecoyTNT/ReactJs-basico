7de574cb9843d53ecc604cf41536b08f805d3dca
DecoyTNT <42523466+DecoyTNT@users.noreply.github.com>
2020-03-31 18:13:05 -0600

    Eventos de react

--- src/components/Producto.js
+++ src/components/Producto.js
@@ -3,10 +3,18 @@ import React from 'react';
 const Producto = ({ producto }) => {
     const { id, nombre, precio } = producto;
 
+    // Agregar producto al carrito
+    const seleccionarProducto = id => {
+        console.log('Comprando ', id);
+    };
+
     return (
         <div>
             <h2>{nombre}</h2>
             <p>${precio}</p>
+            <button type="button" onClick={() => seleccionarProducto(id)}>
+                Comprar
+            </button>
         </div>
     );
 };
